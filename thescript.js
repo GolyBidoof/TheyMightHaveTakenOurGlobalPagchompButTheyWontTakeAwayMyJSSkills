493bc8757353592ff0787c29ac0bee2efd15ef82
Bartłomiej Szymański <szymbar1513@gmail.com>
2019-03-01 20:47:21 +0100

    fixing formatting xD

--- thescript.js
+++ thescript.js
@@ -5,15 +5,15 @@ var intervalID = setInterval(function(){
 		return;
 	} else {    
 		for (var i=0; i<elements.length; i++) {
-            var message = elements[i].innerHTML;
-            if (message == "pagchomp") {
-                var res = message.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
-            } else {
-                  var res = message.replace(/alt=":pagchomp.*\:"/, '');      
-                  var res = res.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
-            }
-            elements[i].innerHTML = res;
-        }
+		    var message = elements[i].innerHTML;
+		    if (message == "pagchomp") {
+			var res = message.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
+		    } else {
+			  var res = message.replace(/alt=":pagchomp.*\:"/, '');      
+			  var res = res.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
+		    }
+		    elements[i].innerHTML = res;
+        	}
 		lastMessage = elements[elements.length-1].innerHTML;
 	}
 }, 300);
