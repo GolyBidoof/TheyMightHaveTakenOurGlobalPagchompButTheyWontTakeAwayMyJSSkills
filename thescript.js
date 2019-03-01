d6ed098fb36a784f20370cd2a87a9b23b261ec2c
Bartłomiej Szymański <szymbar1513@gmail.com>
2019-03-01 20:45:47 +0100

    Now 200% more optimized and 3.3x faster xD

--- thescript.js
+++ thescript.js
@@ -1,13 +1,19 @@
 var elements = document.getElementsByClassName("markup-2BOw-j");
+var lastMessage;
 var intervalID = setInterval(function(){
-    for (var i=0; i<elements.length; i++) {
-        var message = elements[i].innerHTML;
-        if (message == "pagchomp") {
-            var res = message.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
-        } else {
-              var res = message.replace(/alt=":pagchomp.*\:"/, '');      
-              var res = res.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
+	if (lastMessage == elements[elements.length-1].innerHTML) {
+		return;
+	} else {    
+		for (var i=0; i<elements.length; i++) {
+            var message = elements[i].innerHTML;
+            if (message == "pagchomp") {
+                var res = message.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
+            } else {
+                  var res = message.replace(/alt=":pagchomp.*\:"/, '');      
+                  var res = res.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
+            }
+            elements[i].innerHTML = res;
         }
-        elements[i].innerHTML = res;
-}
-}, 1000);
+		lastMessage = elements[elements.length-1].innerHTML;
+	}
+}, 300);
