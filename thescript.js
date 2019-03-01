606f74412277a81a368675d3def7393a928d628c
Bartłomiej Szymański <szymbar1513@gmail.com>
2019-03-01 20:04:54 +0100

    Whoops, I had it running, lol.

--- thescript.js
+++ thescript.js
@@ -2,11 +2,11 @@ var elements = document.getElementsByClassName("markup-2BOw-j");
 var intervalID = setInterval(function(){
     for (var i=0; i<elements.length; i++) {
         var message = elements[i].innerHTML;
-        if (message == "") {
-            var res = message.replace("", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
+        if (message == "pagchomp") {
+            var res = message.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji jumboable">');
         } else {
-              var res = message.replace(//, '');      
-              var res = res.replace("", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
+              var res = message.replace(/alt=":pagchomp.*\:"/, '');      
+              var res = res.replace("pagchomp", '<img src="https://cdn.discordapp.com/emojis/505060647483080735.png" class="emoji">');
         }
         elements[i].innerHTML = res;
 }
